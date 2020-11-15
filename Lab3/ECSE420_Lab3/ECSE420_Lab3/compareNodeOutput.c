f3f166cb6db849844e53bb7b403efb4f993317af
Nicolas <jose.barreyrolopez@mail.mcgill.ca>
2020-11-15 06:20:53 -0500

    global queue done

--- Lab3/ECSE420_Lab3/ECSE420_Lab3/compareNodeOutput.c
+++ Lab3/ECSE420_Lab3/ECSE420_Lab3/compareNodeOutput.c
@@ -2,58 +2,59 @@
 #include <stdlib.h>
 
 
-void compareFiles(char *file_name1, char *file_name2) 
-{ 
-//get from https://www.tutorialspoint.com/c-program-to-compare-two-files-and-report-mismatches
-FILE* fp1 = fopen(file_name1, "r");
-FILE* fp2 = fopen(file_name2, "r");
-    // fetching character of two file 
-    // in two variable ch1 and ch2 
-    char ch1 = getc(fp1); 
-    char ch2 = getc(fp2); 
-  
-    // error keeps track of number of errors 
-    // pos keeps track of position of errors 
-    // line keeps track of error line 
-    int error = 0, pos = 0, line = 1; 
-  
-    // iterate loop till end of file 
-    while (ch1 != EOF && ch2 != EOF) 
-    { 
-        pos++; 
-  
-        // if both variable encounters new 
-        // line then line variable is incremented 
-        // and pos variable is set to 0 
-        if (ch1 == '\n' && ch2 == '\n') 
-        { 
-            line++; 
-            pos = 0; 
-        } 
-  
-        // if fetched data is not equal then 
-        // error is incremented 
-        if (ch1 != ch2) 
-        { 
-            error++; 
-            printf("Line Number : %d \tError"
-               " Position : %d \n", line, pos); 
-        } 
-  
-        // fetching character until end of file 
-        ch1 = getc(fp1); 
-        ch2 = getc(fp2); 
-    } 
-  
-    printf("Total Errors : %d\t", error); 
-} 
+void compareFiles(char* file_name1, char* file_name2)
+{
+	//get from https://www.tutorialspoint.com/c-program-to-compare-two-files-and-report-mismatches
+	FILE* fp1 = fopen(file_name1, "r");
+	FILE* fp2 = fopen(file_name2, "r");
+	// fetching character of two file 
+	// in two variable ch1 and ch2 
+	char ch1 = getc(fp1);
+	char ch2 = getc(fp2);
 
-int main(int argc, char *argv[]){
+	// error keeps track of number of errors 
+	// pos keeps track of position of errors 
+	// line keeps track of error line 
+	int error = 0, pos = 0, line = 1;
 
-    if( argc < 3) {
-      printf("Require two files\n");
-      exit(1);
-      
-   }
-compareFiles(argv[1], argv[2]);
+	// iterate loop till end of file 
+	while (ch1 != EOF && ch2 != EOF)
+	{
+		pos++;
+
+		// if both variable encounters new 
+		// line then line variable is incremented 
+		// and pos variable is set to 0 
+		if (ch1 == '\n' && ch2 == '\n')
+		{
+			line++;
+			pos = 0;
+		}
+
+		// if fetched data is not equal then 
+		// error is incremented 
+		if (ch1 != ch2)
+		{
+			error++;
+			printf("Line Number : %d \tError"
+				" Position : %d \n", line, pos);
+		}
+
+		// fetching character until end of file 
+		ch1 = getc(fp1);
+		ch2 = getc(fp2);
+	}
+
+	printf("Total Errors : %d\t", error);
+}
+
+int main(int argc, char* argv[]) {
+
+	/*if (argc < 3) {
+		printf("Require two files\n");
+		exit(1);
+
+	}*/
+	compareFiles("nodeOutput_global.raw", "sol_nodeOutput.raw");
+	//compareFiles(argv[1], argv[2]);
 }
---
+++ Lab3/ECSE420_Lab3/ECSE420_Lab3/global_queuing.cu
@@ -0,0 +1,190 @@
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include "read_input.h"
+#include "write_output.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define AND     0
+#define OR      1
+#define NAND    2
+#define NOR     3
+#define XOR     4
+#define XNOR    5
+
+typedef struct GateGraph {
+	// Input vars
+	int numNodePtrs;
+	int* nodePtrs_h;
+	int numTotalNeighbors_h;
+	int* nodeNeighbors_h;
+	int numNodes;
+	int* nodeVisited_h;
+	int* nodeGate_h;
+	int* nodeInput_h;
+	int* nodeOutput_h;
+	int numCurrLevelNodes;
+	int* currLevelNodes_h;
+
+	// Output vars
+	int numNextLevelNodes_h; // Queue
+	int* nextLevelNodes_h;
+} GateGraph;
+
+__device__ int computeGate(int values[]) {
+	int compute;
+	int val1 = values[0];
+	int val2 = values[1];
+	int logic = values[2];
+
+
+	if (logic == AND) {
+		compute = val1 & val2;
+	}
+	else if (logic == OR) {
+		compute = val1 | val2;
+
+	}
+	else if (logic == NAND) {
+		compute = !(val1 & val2);
+
+	}
+	else if (logic == NOR) {
+		compute = !(val1 | val2);
+
+	}
+	else if (logic == XOR) {
+		compute = val1 ^ val2;
+
+	}
+	else if (logic == XNOR) {
+		compute = !(val1 ^ val2);
+	}
+
+	return compute;
+}
+
+__global__ void global_queuing_kernel(GateGraph* gate_graph, int num_threads)
+{
+	int idx = threadIdx.x + blockIdx.x * blockDim.x;
+
+	// Iterate through nodes in the current level
+	while (idx < gate_graph->numCurrLevelNodes)
+	{
+		int node = gate_graph->currLevelNodes_h[idx];
+
+		// Iterate through neighbours of the current node
+		for (int neigh_ptr = gate_graph->nodePtrs_h[node]; neigh_ptr < gate_graph->nodePtrs_h[node + 1]; neigh_ptr++)
+		{
+			int neigh = gate_graph->nodeNeighbors_h[neigh_ptr];
+
+#if __CUDA_ARCH__ >= 200
+			// Operate on neighbour if it has not been visited
+			if (!atomicCAS(&(gate_graph->nodeVisited_h[neigh]), 0, 1))
+			{
+				int gate[3] = { gate_graph->nodeInput_h[neigh], gate_graph->nodeOutput_h[node],
+									gate_graph->nodeGate_h[neigh] };
+
+				int nextLevelNodeIdx = atomicAdd(&(gate_graph->numNextLevelNodes_h), 1);	// Increase nextLevelNode index atomically
+				gate_graph->nodeOutput_h[neigh] = computeGate(gate);
+				gate_graph->nextLevelNodes_h[nextLevelNodeIdx] = neigh;
+			}
+#endif
+		}
+
+		idx += num_threads;
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	// Command line
+	/*char* input1_filename = argv[1];
+	char* input2_filename = argv[2];
+	char* input3_filename = argv[3];
+	char* input4_filename = argv[4];
+	char* nodeOutput_filename = argv[5];
+	char* nextLevelNodes_filename = argv[6];*/
+
+	char* input1_filename = "input1.raw";
+	char* input2_filename = "input2.raw";
+	char* input3_filename = "input3.raw";
+	char* input4_filename = "input4.raw";
+	char* nodeOutput_filename = "nodeOutput_global.raw";
+	char* nextLevelNodes_filename = "nextLvlOutput_global.raw";
+
+	// Graph structs (hostmem / devicemem)
+	GateGraph* host_gates;
+	GateGraph* temp_device_gates, * device_gates;
+
+	// Allocate host / device memory
+	host_gates = (GateGraph*)malloc(sizeof(GateGraph));
+	temp_device_gates = (GateGraph*)malloc(sizeof(GateGraph));
+	cudaMalloc((void**)&device_gates, sizeof(GateGraph));
+
+	// Parse input
+	host_gates->numNodePtrs = read_input_one_two_four(&(host_gates->nodePtrs_h), input1_filename);
+	host_gates->numTotalNeighbors_h = read_input_one_two_four(&(host_gates->nodeNeighbors_h), input2_filename);
+	host_gates->numNodes = read_input_three(&(host_gates->nodeVisited_h), &(host_gates->nodeGate_h),
+		&(host_gates->nodeInput_h), &(host_gates->nodeOutput_h), input3_filename);
+	host_gates->numCurrLevelNodes = read_input_one_two_four(&(host_gates->currLevelNodes_h), input4_filename);
+	// Allocate mem for nextLvlNodes (same size as number of nodes - will later count how many nodes there actually are)
+	host_gates->nextLevelNodes_h = (int*)malloc(host_gates->numNodes * sizeof(int));
+	host_gates->numNextLevelNodes_h = 0;
+
+	// Set array sizes in device memory
+	temp_device_gates->numNodePtrs = host_gates->numNodePtrs;
+	temp_device_gates->numTotalNeighbors_h = host_gates->numTotalNeighbors_h;
+	temp_device_gates->numNodes = host_gates->numNodes;
+	temp_device_gates->numCurrLevelNodes = host_gates->numCurrLevelNodes;
+	temp_device_gates->numNextLevelNodes_h = host_gates->numNextLevelNodes_h;
+
+	// Allocate space for arrays in device memory
+	cudaMalloc((void**)&(temp_device_gates->nodePtrs_h), temp_device_gates->numNodePtrs * sizeof(int));
+	cudaMalloc((void**)&(temp_device_gates->nodeNeighbors_h), temp_device_gates->numTotalNeighbors_h * sizeof(int));
+	cudaMalloc((void**)&(temp_device_gates->nodeVisited_h), temp_device_gates->numNodes * sizeof(int));
+	cudaMalloc((void**)&(temp_device_gates->nodeGate_h), temp_device_gates->numNodes * sizeof(int));
+	cudaMalloc((void**)&(temp_device_gates->nodeInput_h), temp_device_gates->numNodes * sizeof(int));
+	cudaMalloc((void**)&(temp_device_gates->nodeOutput_h), temp_device_gates->numNodes * sizeof(int));
+	cudaMalloc((void**)&(temp_device_gates->currLevelNodes_h), temp_device_gates->numCurrLevelNodes * sizeof(int));
+	//Allocate global queue (we do not copy into it)
+	cudaMalloc((void**)&(temp_device_gates->nextLevelNodes_h), temp_device_gates->numNodes * sizeof(int));
+
+	// Copy array contents to device memory
+	cudaMemcpy(temp_device_gates->nodePtrs_h, host_gates->nodePtrs_h, temp_device_gates->numNodePtrs * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(temp_device_gates->nodeNeighbors_h, host_gates->nodeNeighbors_h, temp_device_gates->numTotalNeighbors_h * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(temp_device_gates->nodeVisited_h, host_gates->nodeVisited_h, temp_device_gates->numNodes * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(temp_device_gates->nodeGate_h, host_gates->nodeGate_h, temp_device_gates->numNodes * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(temp_device_gates->nodeInput_h, host_gates->nodeInput_h, temp_device_gates->numNodes * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(temp_device_gates->nodeOutput_h, host_gates->nodeOutput_h, temp_device_gates->numNodes * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(temp_device_gates->currLevelNodes_h, host_gates->currLevelNodes_h, temp_device_gates->numCurrLevelNodes * sizeof(int), cudaMemcpyHostToDevice);
+
+	// Copy struct from host to device
+	cudaMemcpy(device_gates, temp_device_gates, sizeof(GateGraph), cudaMemcpyHostToDevice);
+
+	int blockSize[] = { 32, 64, 128 };
+	int numBlock[] = { 10, 25, 35 };
+
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 3; j++)
+		{
+			global_queuing_kernel << <numBlock[i], blockSize[j] >> > (device_gates, 1024);
+
+		}
+
+	// Copy struct from device to device
+	cudaMemcpy(temp_device_gates, device_gates, sizeof(GateGraph), cudaMemcpyDeviceToHost);
+
+	// Copy nodeOutput and nextLevelNodes from device to host
+	cudaMemcpy(host_gates->nodeOutput_h, temp_device_gates->nodeOutput_h, temp_device_gates->numNodes * sizeof(int), cudaMemcpyDeviceToHost);
+	cudaMemcpy(host_gates->nextLevelNodes_h, temp_device_gates->nextLevelNodes_h, temp_device_gates->numNextLevelNodes_h * sizeof(int), cudaMemcpyDeviceToHost);
+	host_gates->numNextLevelNodes_h = temp_device_gates->numNextLevelNodes_h;
+
+	// Write nodeOutput and nextLevelNodes to files
+	write_output(host_gates->nodeOutput_h, host_gates->numNodes, nodeOutput_filename);
+	write_output(host_gates->nextLevelNodes_h, host_gates->numNextLevelNodes_h, nextLevelNodes_filename);
+
+	return 0;
+}
